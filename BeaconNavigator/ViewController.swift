b9465509a13e48fdbd867eefa050c76c13f9c7eb
Alex Deutsch <alex@MacBook-Pro.fritz.box>
2015-05-18 17:19:44 +0200

    Initial Commit

---
+++ BeaconNavigator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconNavigator
+//
+//  Created by Alex Deutsch on 18.05.15.
+//  Copyright (c) 2015 Alexander Deutsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
